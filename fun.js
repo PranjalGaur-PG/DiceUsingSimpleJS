c21f709a0640f3992f71a13b1aa2818dcdbae2e6
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-01-31 17:08:27 +0530

    first commit

---
+++ fun.js
@@ -0,0 +1,9 @@
+var x = Math.floor(Math.random()*6) +1;
+var y = Math.floor(Math.random()*6) +1;
+
+document.querySelector('.img1').setAttribute("src","images/dice"+x+".png");
+document.querySelector('.img2').setAttribute("src","images/dice"+y+".png");
+
+if(x==y)     document.querySelector('h1').innerHTML = "Draw";
+else if(x>y) document.querySelector('h1').innerHTML = "Player 1 Wins";
+else         document.querySelector('h1').innerHTML = "Player 2 Wins";
